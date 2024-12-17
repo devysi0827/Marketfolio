9fbb581ae4a395c173ab29ea9821ae739134e6cf
devysi0827 <devysi0827@gmail.com>
2024-12-17 17:47:48 +0900

    [chore] 보노

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
 import styled from "styled-components";
+import Bono from "./bo.png";
+// import Bono from "./Hello Bitches Bonobono Food Jjamppong Film PNG - Free Download.jpeg";
 
 function App() {
-  return <Main>효매 바보</Main>;
+  return (
+    <Main>
+      <img src={Bono} height={200} width={400} />
+      <p>효매 바보</p>
+    </Main>
+  );
 }
 
-const Main = styled.p`
+const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
   font-weight: 900;
-  font-size: 32px;
+  font-size: 100px;
+  color: white;
+  gap: 40px;
+
+  p {
+    z-index: 100;
+    -webkit-text-stroke: 4px black;
+  }
+
+  background: linear-gradient(
+    135deg,
+    red,
+    orange,
+    yellow,
+    green,
+    blue,
+    indigo,
+    violet
+  );
 `;
 
 export default App;
